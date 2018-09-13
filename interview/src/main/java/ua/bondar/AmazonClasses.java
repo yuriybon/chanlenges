fa7df13fccbc762157d71bd2fc1957b838a57c5f
bondary <DCTghjcnj_01>
2018-09-13 07:22:44 +0200

    node presenter

--- interview/src/main/java/ua/bondar/AmazonClasses.java
+++ interview/src/main/java/ua/bondar/AmazonClasses.java
@@ -11,45 +11,55 @@ package ua.bondar;
 
 
 public class AmazonClasses {
-     static  Node {
-         private String value;
-
-         public String getValue() {
-             return value;
-         }
-
-         public Node(String value) {
-             this.value = value;
-         }
-
-         Node left;
-         Node right;
-
-         @Override
-         public String toString() {
-
-             return  value + left  + right ;
-
+//     class  NodeTer {
+//         private String value;
+//
+//         public String getValue() {
+//             return value;
+//         }
+//
+//         public Node(String value) {
+//             this.value = value;
+//         }
+//
+//         Node left;
+//         Node right;
+//
+//         public
+//
+//         @Override
+//         public String toString() {
+//
+////             Node  n = this;
+////             String result = "";
+////             String operator = "";
+////             while (n.right != null && n.left != null)  {
+////                 operator = n.right + n.value + n.left;
+////             }
+////
+////             return  result ;
 //             return "Node{" +
 //                     "value='" + value + '\'' +
 //                     ", left=" + left +
 //                     ", right=" + right +
 //                     '}';
-         }
-     }
+//         }
+//     }
+
+
 
     public static void main(String[] args) {
         System.out.println("test");
 
-        Node tree = new Node("-");
-        tree.right = new Node("*");
-        tree.left = new Node("/");
-        tree.right.right = new Node("3");
-        tree.right.left = new Node("2");
-        tree.left.right = new Node("+");
-        tree.left.left = new Node("8");
-        tree.left.right.right = new Node("5");
-        tree.left.right.left = new Node("3");
-        System.out.println(tree);
+//        AmazonClasses.Node tree = new AmazonClasses.Node("-");
+//        tree.right = new Node("*");
+//        tree.left = new Node("/");
+//        tree.right.right = new Node("3");
+//        tree.right.left = new Node("2");
+//        tree.left.right = new Node("+");
+//        tree.left.left = new Node("8");
+//        tree.left.right.right = new Node("5");
+//        tree.left.right.left = new Node("3");
+//        System.out.println(tree);
     }
 }
